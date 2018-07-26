66ec491f93f8c220dcffc4fba081b2083f4a295b
chenwx <1061830945@qq.com>
2018-07-26 13:33:51 +0800

    添加单元测试依赖以及demo

---
+++ src/test/java/bingosoft/hrhelper/service/test.java
@@ -0,0 +1,26 @@
+package bingosoft.hrhelper.service;
+
+import bingosoft.hrhelper.mapper.UserMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @创建人 chenwx
+ * @功能描述
+ * @创建时间 2018-07-26 12:27:27
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class test {
+
+    @Autowired
+    UserMapper userMapper;
+    @Test
+    public void test(){
+        System.out.println(userMapper);
+        System.out.print("这是单元测试");
+    }
+}
