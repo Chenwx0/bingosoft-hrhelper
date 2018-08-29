8dcc5f1eb1bf0f7ad4147f865719d6290b2bea0b
hancc@bingosoft.net <15202436921@163.com>
2018-08-29 23:17:07 +0800

    修改模板替换bug

--- src/main/java/bingosoft/hrhelper/service/CreateMailContentService.java
+++ src/main/java/bingosoft/hrhelper/service/CreateMailContentService.java
@@ -5,18 +5,17 @@ import bingosoft.hrhelper.mapper.EmployeeMapper;
 import bingosoft.hrhelper.mapper.ModelMapper;
 import bingosoft.hrhelper.model.Employee;
 import bingosoft.hrhelper.model.Model;
-
 import org.joda.time.DateTime;
 import org.joda.time.Months;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -42,6 +41,8 @@ public class CreateMailContentService {
     //发送时间
     private String sendDate = "convert#SendDate#";
 
+    private Random random = new Random();
+
 
     @Autowired
     EmployeeMapper employeeMapper;
@@ -51,8 +52,30 @@ public class CreateMailContentService {
     public String getMailContent(String userId, String mailModelId, String sendTime, String deadDate){
         Employee employee = employeeMapper.selectByPrimaryKey(userId);
         Model model = modelMapper.selectByPrimaryKey(mailModelId);
+        deadDate = "";
         //获取邮件模板内容
         String content = model.getModelContent();
+
+        //替换"WorkMonth"
+        if (content.contains(workMath)){
+            content = replaceWorkMonth(employee, content);
+        }
+        //替换“截至日期”
+        if (deadDate.equals("")){
+            SimpleDateFormat sdFormat=new SimpleDateFormat("yyyy年MM月dd日");
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(formatDate(sendTime));
+            calendar.add(Calendar.DATE, random.nextInt(10)+1);
+            deadDate = sdFormat.format(calendar.getTime());
+        }
+        if (content.contains(deadline)){
+            content = replaceDeadline(deadDate, content);
+        }
+        //替换“发送时间”
+        if (content.contains(sendDate)){
+            content = replaceSendDate(sendTime, content);
+        }
+
         Matcher matcher = Pattern.compile(regex).matcher(content);
         //map用来存放邮件模板可直接进行替换的值对
         Map<String, String> map = new HashMap<>();
@@ -60,6 +83,10 @@ public class CreateMailContentService {
             String key = matcher.group(0) + "#";
             String keyEntry = formatFiled(key);
             String value = employee.getValue(keyEntry.substring(8,keyEntry.length()-1));
+            if (value.contains(" CST ")){
+                Date date = formatDate(value);
+                value = DateFormat.getDateInstance(DateFormat.FULL).format(date).split(" ")[0];
+            }
             if (!value.equals("")){
                 map.put(key,value);
             }
@@ -68,18 +95,6 @@ public class CreateMailContentService {
         for (String key : map.keySet()){
             String value = map.get(key);
             content = content.replaceAll(key,value);
-        }
-        //替换"WorkMonth"
-        if (content.contains(workMath)){
-            content = replaceWorkMonth(employee, content);
-        }
-        //替换“截至日期”
-        if (deadline.equals("") && content.contains(deadline)){
-            content = replaceDeadline(deadDate, content);
-        }
-        //替换“发送时间”
-        if (content.contains(sendDate)){
-            content = replaceSendDate(sendTime, content);
         }
         //返回结果
         return content;
@@ -132,5 +147,63 @@ public class CreateMailContentService {
             i++;
         }
         return newFiled.toString();
+    }
+
+    //时间格式转换
+    private Date formatDate(String string) {
+        SimpleDateFormat resultSdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        SimpleDateFormat resultSdfdate = new SimpleDateFormat("yyyy-MM-dd");
+        if (string != null) {
+            if (string.contains("CST")) {
+                long d2 = Date.parse(string);
+                Date datetime = new Date(d2);
+
+                return datetime;
+
+            } else if (string.contains("Z")) {
+                SimpleDateFormat sdf = new SimpleDateFormat(
+                        "yyyy-MM-dd'T'hh:mm:ss'.'sss'Z'");
+                java.util.Date datetime;
+                try {
+                    datetime = sdf.parse(string);
+                    return (Date) datetime;
+                } catch (ParseException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            } else if (string.contains("-")&&string.contains(":")) {
+                Date newDate;
+                try {
+                    newDate = resultSdf.parse(string);
+                    return newDate;
+                } catch (ParseException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            } else if(string.contains("-")&&!string.contains(":")){
+                Date newDate;
+                try {
+                    newDate = resultSdfdate.parse(string);
+                    return newDate;
+                } catch (ParseException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }else {
+                Date longDate = new Date(Long.parseLong(string));
+
+                return longDate;
+            }
+        }
+        return null;
+    }
+
+    //calendat转date
+    public static Date calendarToData(int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();//日历类的实例化
+        calendar.set(year, month - 1, day);//设置日历时间，月份必须减一
+        Date date = calendar.getTime(); // 从一个 Calendar 对象中获取 Date 对象
+        return date;
     }
 }
